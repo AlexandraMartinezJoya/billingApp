bd11f8e3723a403e61e85606a61e265033cefad3
Alexa <za0ne78@hotmail.com>
2016-12-21 20:08:33 +0200

    Update common.php

--- common.php
+++ common.php
@@ -1,15 +1,11 @@
-<?php
-
+<?php 
 include 'projectName.xml';
 include 'errorHandling.php';
 include '../../db.php';
-
 //debugging function. prints selected whatever stateless obj in a somewhat more formatted xml way
 function prePrint($obj){
-    echo "<pre>".print($obj,true)."</pre>";
+    echo "<pre>". print($obj)."</pre>";
 }
-
-
 //function that returns the data hold in the unicodeUFT8Data until it hits the numeric value held at $integerValue
 function stringSlice ($unicodeUFT8Data, $integer) {
     //...todo for tomorrow **note***http://www.utf8-chartable.de/ . also should find another site to double check those germans
@@ -30,42 +26,37 @@ function ifXml($obj){
 }
 
 function ifDependencyisXML($obj){
-    ifXml($obj){
-        testInput($obj)
+    if(null !== ifXml($obj)){
+        ifXml($obj);
+        testInput($obj);
         return true;
     }
-    else {
-    echo "not xml" // perhaps perform another check for other types of builds, like json encoding;
-    echo ($obj);
+ else {
+    echo "not xml"; // perhaps perform another check for other types of builds, like json encoding;
+    prePrint ($obj);
     testInput($obj);
     return false;
     }
 }
-
 //function to check the file version corresponds with the project dependency, if dependency is xml
 function fileVersion($obj) {
-	echo "<?xml version='".print($obj, true)."' encoding='UTF-8'?>";
+	echo "<?xml version=".print($obj) ."encoding='UTF-8'?>";
 }
-
 //function to change the name of the project dependency,if dependency is xml
 function insertNameTag($obj) {
-	echo "<name>".print($obj, true)."</name>";
+	echo "<name>".print($obj)."</name>";
 }
-
 //function to change the file version of the project dependency is xml
 function insertCommentTag($obj) {
-	echo "<comment>".print($obj, true)."</comment>";
+	echo "<comment>".print($obj)."</comment>";
 }
-
 //function to chage the project tag of the project dependency , if dependency is xml
 function insertProjectsTag($obj) {
-	echo "<projects>".print($obj, true)."</projects>";
+	echo "<projects>".print($obj)."</projects>";
 }
-
 function insertBuildSpecTag($obj) {
-	echo "<buildSpec>". print($obj, true). "</buildSpec>";
+	echo "<buildSpec>". print($obj). "</buildSpec>";
 }
-
 // create a db handler with PDO lib. note(alexa) please look into depth here. Much might come to a 2nd db
 global $dataBaseHandler;
 $dataBaseHandler = new PDO($DB_HOST, $DB_USER, $DB_PASS);
@@ -73,14 +64,12 @@ $dataBaseHandler = new PDO($DB_HOST, $DB_USER, $DB_PASS);
         echo "Connected!";
     }
 $dataBaseHandler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 //REST GLOBALs types, methods to check if the data inputed is passed correctly, we need our flow :)
 prePrint($_POST);
 prePrint($_PUT);
 prePrint($_GET);
 prePrint($_SESSION);
 prePrint($_CATCHE);
-
 //trim, and bare minimum of data sanitization
 function testInput($obj) {
   $obj = trim($obj);
@@ -89,9 +78,6 @@ function testInput($obj) {
   return $obj;
 }
 
-function ass() {
-    echo "as";
-}
 
 //function that sorts 2 arrays and checks if they are equal
 function identicalValues( $dataset1 , $dataset2 )
@@ -101,17 +87,26 @@ function identicalValues( $dataset1 , $dataset2 )
     return $dataset1 == $dataset2;
 }
 
+
+
+function ifNull($obj) 
+{
+    if ($obj == NULL) 
+        return true;
+    else 
+        return false;       
+}
 //function that checks if struct1 is different from struct2
-function remaingorn($struct1, $struct2){
-    if(isset($struct1) && isset($struct2) {
+function remaingorn($struct1, $struct2) {
+    if(ifNull($struct1) && ifNull($struct2)) {
         // check to see if it actually prints the data, and how it looks like :)
         echo ($struct1);
         echo($struct2);
-	$plusCount = $minusCount = 0;
+	    $plusCount = $minusCount = 0;
         testInput($struct1);
         testInput($struct2);
-        foreach($struct1 ass() $dataSize1 => $numberOfBits1) {
-            foreach($struct2 ass() $dataSize2 => $numberOfBits2)
+        foreach($struct1 as $dataSize1 => $numberOfBits1) {
+            foreach($struct2 as $dataSize2 => $numberOfBits2)
             {
                 if(!identicalValues($numberOfBits1, $numberOfBits2))
                     
@@ -119,14 +114,18 @@ function remaingorn($struct1, $struct2){
 		    	$plusCount =  $numberOfBits1 + $numberOfBits2;
             }
         }
-        if (($minusCount + $plusCount) === 0 ){    
-            return echo "these Data Sets are equals"; 
-	} else if (($minusCount + $plusCount) > 0 ) {
-	    return echo "The Plus Count is the bigger Data Set"; 
-	} else { 
-	    return echo "The Minus Count is the Bigger Data Set";		
-	}
-  }	
+
+    	if (($minusCount + $plusCount) > 0 ) {
+	        echo "The Plus Count is the bigger Data Set, returns a pozitive value"; 
+            return 1;
+        } else if (($minusCount + $plusCount) < 0) { 
+            echo "The Minus Count is the Bigger Data Set, returns a negative value";
+	        return -1;		
+	    } else if (($minusCount + $plusCount) === 0 ){    
+            echo "the structures might be identical, a sort and a recussion is in need here";
+            return 0; 
+        }	
+    }
 }
 
 ?>
