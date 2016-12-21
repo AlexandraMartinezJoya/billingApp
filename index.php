747f7b91192dbb9cf585181779a09847afbaead2
Alexa <za0ne78@hotmail.com>
2016-12-21 19:34:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <!DOCTYPE HTML>
+<html lang="en">
+
+  <head>
+      <!-- Latest compiled and modified CSS -->
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="./font-awesome-4.6.3/css/font-awesome.min.css">
+
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
