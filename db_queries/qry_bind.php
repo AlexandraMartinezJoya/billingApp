916e689631bc87de668b66e57f04fa79dd31d8f1
Alexa <za0ne78@hotmail.com>
2017-01-05 14:44:45 +0200

    Update qry_bind.php

--- db_queries/qry_bind.php
+++ db_queries/qry_bind.php
@@ -1,78 +1,73 @@
 <?php
-    //You can include my name in the header file, for apparently that's the proper code. za0ne101 :)
-    //this is where one can include a simple file that stores the db connect variables
-    // for example :
-    
-    //$DB_HOST = 'mysql:host=localhost;dbname=facturi';
-    //$DB_USER = 'root';
-    //$DB_PASS = '';
-    include "filePathToDbStore.php";
 
 
-    //function to return all the facturi found in db. returns me
-    function getAllFacturi() {
-        global $dbh;
-        $stmt_f = $dbh->prepare('SELECT * FROM facturi');
+$DB_HOST = 'mysql:host=localhost;dbname=facturi';
+$DB_USER = 'root';
+$DB_PASS = '';
+
+    //function to return all the bills found in db
+    function getAllBills() {
+        global $dataBaseHandler;
+        $stmt_f = $dataBaseHandler->prepare('SELECT * FROM facturi');
         $stmt_f -> execute();
         $facturi = $stmt_f ->fetchAll();
         return $facturi;
     }
 
     //function to return the factura data searched after the id of the item. returns an array
-    function getFactura($id_fact){
-        global $dbh;
-        $stmt_idf = $dbh->prepare('SELECT * from facturi WHERE id = :id');
-        $stmt_idf->bindParam(':id', $id_fact);
+    function getBillData($idBill){
+        global $dataBaseHandler;
+        $stmt_idf = $dataBaseHandler->prepare('SELECT * from facturi WHERE id = :id');
+        $stmt_idf->bindParam(':id', $idBill);
         $stmt_idf -> execute();
         $facturi_gasite = $stmt_idf ->fetch(PDO::FETCH_ASSOC);
         return $facturi_gasite;
     }
 
     // function to select a field based on table col name and id, returns the aassociated data
-    function get_fact_table_data($id_fact, $table_col_name) {
-    $data = getFactura($id_fact);
-    $denum = $data[$table_col_name];
+    function getTableBillData($idBill, $tableColName) {
+    $data = getFactura($idBill);
+    $denum = $data[$tableColName];
     return $denum;
     }
 
-    // gets all produse data containing in db. this is not that optimal if the db is going to be really big. I don't  know where i use this but i think it
-    // be changed.
-    function getAllProduse() {
-        global $dbh;
-        $stmt = $dbh->prepare('SELECT * FROM produs');
+    // gets all products data from in db. this is not that optimal if the db is going to be really big. 
+        function getAllProducts() {
+        global $dataBaseHandler;
+        $stmt = $dataBaseHandler->prepare('SELECT * FROM produs');
         $stmt -> execute();
-        $produse = $stmt -> fetchAll();
-        return $produse;
+        $products = $stmt -> fetchAll();
+        return $products;
     }
 
 
     //gets data from produs table from db , returns an associtive array based on a forgein key select
-    function getAllProdus($fk_id_fact) {
-        global $dbh;
-        $stmt_p = $dbh->prepare('SELECT * from produs WHERE id_fact = :id_fact');
-        $stmt_p ->bindParam(':id_fact', $fk_id_fact);
+    function getAllProductsByForgeinKey($fkIDFact) {
+        global $dataBaseHandler;
+        $stmt_p = $dataBaseHandler->prepare('SELECT * from produs WHERE id_fact = :id_fact');
+        $stmt_p ->bindParam(':id_fact', $fkIDFact);
         $stmt_p -> execute();
-        $produse = $stmt_p ->fetchAll();
-        return $produse;
+        $forgeinKeyProducts = $stmt_p ->fetchAll();
+        return $forgeinKeyProducts;
     }
 
     // function insert_benef_name_data($denumire) {
-    //     global $dbh;
+    //     global $dataBaseHandler;
     //     preprint($denumire);
     //     $sql = "INSERT INTO facturi (benef_denumire) VALUES (:benef_denum)";
-    //     $stmt = $dbh ->prepare($sql);
+    //     $stmt = $dataBaseHandler ->prepare($sql);
     //     $stmt -> bindParam(':benef_denum', $denumire);
     //     $stmt -> execute();
     //     echo "done";
     // }
 
 
-    // nnserts fact data to the db , takes in arguments as variables for each field
-    function insert_benef_fact_data($denumire, $cui, $adresa, $cont_bancar, $data_emiterii, $data_scadenta, $serie, $numar) {
-         global $dbh;
+    // inserts fact data to the db , takes in arguments as variables for each field
+    function insertBeneficiaryBillData($denumire, $cui, $adresa, $cont_bancar, $data_emiterii, $data_scadenta, $serie, $numar) {
+         global $dataBaseHandler;
          $sql = "INSERT INTO facturi (benef_denumire, benef_cui, benef_adresa, benef_cont_bancar, data_emiterii, data_scadentei, serie, nr)
              VALUES (:benef_denum, :benef_cui, :benef_adresa, :benef_cont_bancar, :data_emiterii, :data_scadentei, :serie, :numar)";
-         $stmt = $dbh ->prepare($sql);
+         $stmt = $dataBaseHandler ->prepare($sql);
          preprint($stmt);
          $stmt -> bindParam(':benef_denum', $denumire);
          $stmt -> bindParam(':benef_cui', $cui);
@@ -84,11 +79,12 @@
          $stmt -> bindParam(':numar', $numar);
          $stmt -> execute();
 //         echo  'records inserted successfully'."<br>";
-         $id = $dbh->lastInsertId($sql);
+         $id = $dataBaseHandler->lastInsertId($sql);
 //         preprint($id);
          return $id;
      }
 
+        // Code sample by Stefancu
         //  function handle_produse_post() {
          //
         //      $produse = filter_input('POST', 'produse', );
@@ -105,7 +101,7 @@
 
 
     // insert prepared statement for data in produs. takes post data as a matrix and sends it to the forgein key relation
-    function insert_prod_data($prod_array, $fact_id) {
+    function insertProductData($prod_array, $fact_id) {
             foreach ($prod_array as $produs) {
                 insert_row_data($produs, $fact_id);
             }
@@ -114,9 +110,9 @@
 
 
     // insets table data in produs table, 1 row. constraint is that you need the row data stored as an array, and the forgein key id of the fact related to it
-    function insert_row_data($row_Array, $fk_id) {
-        global $dbh;
-        $stmt = $dbh ->prepare('INSERT INTO produs (denumire, unit_masura, cantitate, pret_unitar, id_fact) VALUES(:prod_denum, :prod_um, :prod_pu, :prod_q, :id_fact)');
+    function insertProductRowData($row_Array, $fk_id) {
+        global $dataBaseHandler;
+        $stmt = $dataBaseHandler ->prepare('INSERT INTO produs (denumire, unit_masura, cantitate, pret_unitar, id_fact) VALUES(:prod_denum, :prod_um, :prod_pu, :prod_q, :id_fact)');
         $stmt -> bindParam(':id_fact', $fk_id);
         $stmt -> bindParam(':prod_denum', $row_Array["prod_denum"]);
         $stmt -> bindParam(':prod_um', $row_Array["prod_um"]);
@@ -128,10 +124,10 @@
 
 
     // updates the data in factura table, constraint is that the table data is stored in variables
-    function update_fact_data($benefDenumire, $benefCui, $benefContBancar, $benefAdresa, $benefDataEmiterii, $benefDataScadenta, $factSerie, $factNr, $id_fact) {
+    function updateBillData($benefDenumire, $benefCui, $benefContBancar, $benefAdresa, $benefDataEmiterii, $benefDataScadenta, $factSerie, $factNr, $id_fact) {
         //code here... repeating quite a bit, you could reduce the code significantly by applying the param binding in a array
-        global $dbh;
-        $stmt = $dbh ->prepare('UPDATE facturi  SET benef_denumire = :benef_denumire, benef_cui = :benef_cui, benef_cont_bancar = :benef_cont_bancar,
+        global $dataBaseHandler;
+        $stmt = $dataBaseHandler ->prepare('UPDATE facturi  SET benef_denumire = :benef_denumire, benef_cui = :benef_cui, benef_cont_bancar = :benef_cont_bancar,
                                                     benef_adresa = :benef_adresa, data_emiterii = :data_emiterii, data_scadentei = :data_scadentei,
                                                     serie = :serie, nr = :nr
                                                 WHERE id = :id');
@@ -150,15 +146,15 @@
 
 
     // updates data in produs table , with a forgein key match constraint
-    function update_prod_data($produse_array){
+    function updateProductData($produse_array){
         foreach ($produse_array as $produs) {
             update_row_data($produs);
         }
     }
 
-        function update_row_data($produs_array){
-            global $dbh;
-            $stmt = $dbh -> prepare('UPDATE produs SET denumire = :prod_denum, unit_masura  = :prod_um, cantitate = :prod_q, pret_unitar = :prod_pu
+        function updateRowData($produs_array){
+            global $dataBaseHandler;
+            $stmt = $dataBaseHandler -> prepare('UPDATE produs SET denumire = :prod_denum, unit_masura  = :prod_um, cantitate = :prod_q, pret_unitar = :prod_pu
                                         WHERE id = :id');
             $stmt -> bindParam(':id', $produs_array["id"]);
             $stmt -> bindParam(':prod_denum', $produs_array["prod_denum"]);
@@ -170,10 +166,10 @@
             }
 
     // Selects data from db tables to return the multiplication of two fields, using a forgein key id.
-    function select_total_per_fact() {
+    function selectTotalPerBill() {
         //code here..
-        global $dbh;
-        $stmt = $dbh -> prepare('SELECT p.id_fact, SUM( p.cantitate * p.pret_unitar) AS tg FROM produs p, facturi f WHERE p.id_fact = f.id GROUP BY f.id;');
+        global $dataBaseHandler;
+        $stmt = $dataBaseHandler -> prepare('SELECT p.id_fact, SUM( p.cantitate * p.pret_unitar) AS tg FROM produs p, facturi f WHERE p.id_fact = f.id GROUP BY f.id;');
         $stmt ->execute();
         $total_general = $stmt-> fetchall();
         return $total_general;
@@ -181,9 +177,9 @@
     }
 
     //Deletes data from db table name produs , on id select
-    function delete_prod_data($prod_id, $prod_array) {
-        global $dbh;
-        $stmt = $dbh -> prepare("DELETE FROM produs WHERE id =:id");
+    function deleteProductData($prod_id, $prod_array) {
+        global $dataBaseHandler;
+        $stmt = $dataBaseHandler -> prepare("DELETE FROM produs WHERE id =:id");
         $stmt -> bindParam(':id', $prod_id);
         $stmt -> execute();
 //        echo "</br>records deleted.</br>";
