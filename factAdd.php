a456234dc98cfd9b1cb50add51ae4ae86aa66bda
Alexa <za0ne78@hotmail.com>
2017-01-07 01:31:18 +0200

    Create factAdd.php

---
+++ factAdd.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+<!-- coding standards now requites to mention my name if you use this. @AlexandraMartinez -->
+
+      <!-- Bootstrap and FA CSS -->
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="./font-awesome-4.6.3/css/font-awesome.min.css">
+
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+      <!-- Bootstrap JavaScript -->
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+<body>
+
+
+</body>
